4dc850863f1ac8c41be74f0286cf48e086882a34
nikitas-k <31430935+nikitas-k@users.noreply.github.com>
2017-09-12 22:22:20 +1000

    Add files via upload

--- MNE_revision.py
+++ MNE_revision.py
@@ -10,39 +10,39 @@
 import Tkinter
 import tkFileDialog
 root = Tkinter.Tk()
-filename = tkFileDialog.askopenfilename(parent=root, initialdir='/users/nikitaskoussis/Dropbox/Science/PVB304/Article/MNE', 
+fname = tkFileDialog.askopenfilename(parent=root, initialdir='/users/nikitaskoussis/Dropbox/Science/PVB304/Article/MNE', 
                                     title='Please select your EEG file')
 root.destroy()
 
 
 # ## Import the relevant libraries
 
-# In[5]:
+# In[2]:
 
 import mne
 import scipy
-import numpy 
+import numpy as np
 import matplotlib
 matplotlib.use('Qt5Agg')
 
 
 # ## Read the raw file
 
-# In[112]:
+# In[3]:
 
-raw = mne.io.read_raw_eeglab(filename)
+raw = mne.io.read_raw_eeglab(fname)
 print(raw)
 print(raw.info)
 
 
-# In[5]:
+# In[4]:
 
 print(raw.ch_names)
 
 
 # ### Preprocessing for speed (may not be needed)
 
-# In[78]:
+# In[5]:
 
 #tmin, tmax = 20, 200
 #raw.crop(tmin, tmax).load_data()  # 20s to 200s data segment for speed
@@ -51,7 +51,7 @@ print(raw.ch_names)
 
 # ### Designating bad channels
 
-# In[113]:
+# In[4]:
 
 # bad channels
 raw.info['bads'] = ['REF','E82','E102','E103','E111','E112','E113','E120','E121',
@@ -61,7 +61,9 @@ picks = mne.pick_types(raw.info, meg=False, eeg=True, eog=False, stim=False,
                        exclude='bads')
 
 
-# In[143]:
+# ### Sensor mapping
+
+# In[5]:
 
 raw.plot_sensors() #plotting the sensor map in 2d
 raw.plot_sensors(kind='3d', ch_type='eeg', ch_groups='position') #sensor map in 3d
@@ -69,28 +71,28 @@ raw.plot_sensors(kind='3d', ch_type='eeg', ch_groups='position') #sensor map in 
 
 # ### Projection mapping
 
-# In[114]:
+# In[6]:
 
 mne.set_eeg_reference(raw) #prevent MNE from setting reference automatically
 
 
 # ### Power plot
 
-# In[81]:
+# In[9]:
 
 raw.plot_psd(average=False)
 
 
 # ### Filtering of data
 
-# In[116]:
+# In[8]:
 
-raw.filter(l_freq=None, h_freq=50) #low-pass filter at 50 Hz
+raw.filter(l_freq=None, h_freq=55) #low-pass filter at 55 Hz so auditory triggers aren't cut off
 raw.filter(l_freq=1, h_freq=None) #high-pass filter at 1 Hz
-raw.notch_filter(freqs=[50,100], picks=picks, filter_length='auto', phase='zero')
+raw.notch_filter(freqs=[50,100], picks=picks, filter_length='auto', phase='zero') # run cell twice
 
 
-# In[84]:
+# In[9]:
 
 raw.plot_psd(average=False)
 raw.plot()
@@ -98,20 +100,21 @@ raw.plot()
 
 # ### Finding events
 
-# In[136]:
+# In[10]:
 
 events = mne.find_events(raw, stim_channel='STI 014', shortest_event=1)
-event_id = {'lett' : 110, 'aud_l' : 137, 'aud_r' : 138, 'go/nogo' : 131, 'vis' : 88}
+event_id = {'lett' : 110, 'aud_l' : 137, 'aud_r' : 138, 'vis_l' : 87, 'vis_r' : 88}
 print(event_id)
+mne.write_events(event_list=events, filename='loc_out_noepi-eve.fif')
 
 
 # ### Event visualisation
 
-# In[122]:
+# In[11]:
 
 # Plot the events to get an idea of the paradigm
 # Specify colors
-color = {131: 'green', 138: 'yellow', 137: 'red', 110: 'black', 88: 'c'}
+color = {87: 'green', 138: 'yellow', 137: 'red', 110: 'black', 88: 'c'}
 
 mne.viz.plot_events(events, raw.info['sfreq'], raw.first_samp, color=color,
                     event_id=event_id)
@@ -119,14 +122,14 @@ mne.viz.plot_events(events, raw.info['sfreq'], raw.first_samp, color=color,
 
 # ### Setting baseline
 
-# In[123]:
+# In[12]:
 
 baseline = (None, 0)  # means from the first instant to t = 0
 
 
 # ### Peak-to-peak rejection parameters
 
-# In[124]:
+# In[13]:
 
 reject = dict(eeg=200e-6)
 
@@ -135,7 +138,7 @@ reject = dict(eeg=200e-6)
 
 # ### Visual trigger
 
-# In[125]:
+# In[14]:
 
 # event trigger and conditions
 tmin = -0.2  # start of each epoch (200ms before the trigger)
@@ -143,53 +146,98 @@ tmax = 0.5  # end of each epoch (500ms after the trigger)
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                   baseline=baseline, preload=True, reject=reject)
 epochs.drop_bad()
-epochs_vis = epochs.pick_channels(['E9','E45','E186','E8','E17','E24',
+epochs_vis_l = epochs.pick_channels(['E9','E45','E186','E8','E17','E24',
                                   'E43','E52','E53','E80','E90','E81','E132','E157','E148',
                                 'E137','E125','E149','E116']) #Setting channels for 
                                           #evoked response of Visual trigger
-evoked_vis = epochs_vis['vis'].average()
-evoked_vis.plot(titles='Visual feedback evoked response', spatial_colors=True, gfp=True) #Plotting evoked response
-                                                                                      # of visual channels
-evoked_vis.plot_topomap()
+evoked_vis_l = epochs['vis_l'].average()
+evoked_vis_l.plot(titles='Visual (flashing left) feedback evoked response', spatial_colors=True, gfp=True) 
+#Plotting evoked response of visual channels
+
+
+# In[15]:
+
+mne.write_evokeds(evoked=evoked_vis_l,fname='loc_out_noepi_vis_l-ave.fif')
 
 
 # #### More visualisation
 
-# In[126]:
+# In[16]:
 
+evoked_vis_l.plot_topomap(times=np.arange(-0.1, 0.4, 0.05))
 ts_args = dict(gfp=True)
 topomap_args = dict(sensors=False)
 
-evoked_vis.plot_joint(title='Visual evoked response', times = [.0, 0.075, 0.125, 0.25],
+evoked_vis_l.plot_joint(title='Visual (flashing left) evoked response', times = [.0, 0.075, 0.125, 0.25],
                       ts_args=ts_args, topomap_args=topomap_args)
 
 
-# ### "Letter"  trigger
+# In[18]:
 
-# In[142]:
+# event trigger and conditions
+tmin = -0.2  # start of each epoch (200ms before the trigger)
+tmax = 0.5  # end of each epoch (500ms after the trigger)
+epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
+                   baseline=baseline, preload=True, reject=reject)
+epochs.drop_bad()
+epochs_vis_r = epochs.pick_channels(['E9','E45','E186','E8','E17','E24',
+                                   'E43','E52','E53','E80','E90','E81','E132','E157','E148',
+                                 'E137','E125','E149','E116']) #Setting channels for 
+                                           #evoked response of Visual trigger
+evoked_vis_r = epochs['vis_r'].average()
+evoked_vis_r.plot(titles='Visual (flashing right) feedback evoked response', spatial_colors=True, gfp=True) #Plotting evoked response
+                                                                                       # of visual channels
+
+
+# In[19]:
+
+evoked_vis_r.plot_topomap(times=np.arange(-0.1, 0.4, 0.05))
+ts_args = dict(gfp=True)
+topomap_args = dict(sensors=False)
+
+evoked_vis_r.plot_joint(title='Visual (flashing right) evoked response', times = [.0, 0.075, 0.125, 0.25],
+                      ts_args=ts_args, topomap_args=topomap_args)
+
+
+# In[30]:
+
+left, right = epochs_vis_l["vis_l"].average(), epochs_vis_r["vis_r"].average()
+
+# create and plot difference ERP
+mne.combine_evoked([left, -right], weights='equal').plot_joint(times=np.arange(-0.2,0.5,0.15))
+
+
+# ### "Letter"  inhibition trigger
+
+# In[20]:
 
 tmin = -0.5  # start of each epoch (200ms before the trigger)
 tmax = 1  # end of each epoch (1000ms after the trigger)
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
                     baseline=baseline, preload=True, reject=reject)
 epochs.drop_bad()
-epochs_lett = epochs.pick_channels(['E9','E45','E186','E8','E17','E24',
-                                    'E43','E52','E53','E80','E90','E81','E132','E157','E148',
-                                  'E137','E125','E149','E116'])
+epochs_lett = epochs.pick_channels(['E31','E32','E26','E25','E18','E19','E20','E27'
+                                  'E33','E37','E9','E8','E24',
+                                'E43','E52','E53','E80','E90','E81','E132','E157','E148',
+                                  'E137','E125','E149','E116','E45','E186','E17'])
 evoked_lett = epochs_lett['lett'].average()
-evoked_lett.plot(titles='Button evoked response', spatial_colors=True, gfp=True)
-evoked_lett.plot_topomap(title='Button evoked response power map over time')
+evoked_lett.plot(titles='Button go/nogo evoked response', spatial_colors=True, gfp=True)
 
 
-# In[141]:
+# In[34]:
 
-evoked_lett.plot_joint(title='Button evoked response', times = [0, 0.5, 1],
+mne.write_evokeds(evoked=evoked_lett,fname='loc_out_noepi_lett-ave.fif')
+
+
+# In[21]:
+
+evoked_lett.plot_joint(title='Button go/nogo evoked response', times = np.arange(-0.4, 1, 0.2),
                       ts_args=ts_args, topomap_args=topomap_args)
 
 
 # ### Auditory/Left
 
-# In[152]:
+# In[22]:
 
 tmin = -0.02  # start of each epoch (20ms before the trigger)
 tmax = 0.075 # end of each epoch (75ms after the trigger)
@@ -201,18 +249,22 @@ epochs_aud_l = epochs.pick_channels(['E43','E52','E53','E80','E90','E81','E251'
                                      'E216','E233','E209','E232','E219'])
 evoked_aud_l = epochs_aud_l['aud_l'].average()
 evoked_aud_l.plot(titles='Auditory/Left evoked response', spatial_colors=True, gfp=True)
-evoked_aud_l.plot_topomap()
 
 
-# In[138]:
+# In[23]:
 
-evoked_aud_l.plot_joint(title='Auditory/Left evoked response', times = [0, 0.018, 0.033],
+mne.write_evokeds(evoked=evoked_aud_l, fname='loc_out_noepi_aud_l-ave.fif')
+
+
+# In[24]:
+
+evoked_aud_l.plot_joint(title='Auditory/Left evoked response', times = np.arange(-0.02, 0.075, 0.02),
                       ts_args=ts_args, topomap_args=topomap_args)
 
 
 # ### Auditory/Right
 
-# In[153]:
+# In[25]:
 
 tmin = -0.02  # start of each epoch (20ms before the trigger)
 tmax = 0.075  # end of each epoch (75ms after the trigger)
@@ -225,39 +277,25 @@ epochs_aud_r = epochs.pick_channels(['E43','E52','E53','E80','E90','E81','E251'
                                      'E216','E233','E209','E232','E219'])
 evoked_aud_r = epochs_aud_r['aud_r'].average()
 evoked_aud_r.plot(titles='Auditory/Right evoked response', spatial_colors=True, gfp=True)
-evoked_aud_r.plot_topomap()
 
 
-# In[140]:
+# In[26]:
 
-evoked_aud_r.plot_joint(title='Auditory/Right evoked response', times = [0, 0.018, 0.033],
+mne.write_evokeds(evoked=evoked_aud_r, fname='loc_out_noepi_aud_r-ave.fif')
+
+
+# In[27]:
+
+evoked_aud_r.plot_joint(title='Auditory/Right evoked response', times = np.arange(-.02, 0.075, 0.02),
                       ts_args=ts_args, topomap_args=topomap_args)
 
 
 # ### Average of Auditory trials
 
-# In[154]:
+# In[28]:
 
 left, right = epochs_aud_l["aud_l"].average(), epochs_aud_r["aud_r"].average()
 
 # create and plot difference ERP
-mne.combine_evoked([left, -right], weights='equal').plot_joint(times=[0,0.018,0.025,0.033])
-
-
-# ### Go/nogo inhibition task
-
-# In[148]:
-
-tmin = -0.5  # start of each epoch (200ms before the trigger)
-tmax = 1  # end of each epoch (1000ms after the trigger)
-epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
-                    baseline=baseline, preload=True, reject=reject)
-epochs.drop_bad()
-epochs_go = epochs.pick_channels(['E31','E32','E26','E25','E18','E19','E20','E27'
-                                  'E33','E37','E9','E45','E186','E8','E17','E24',
-                                'E43','E52','E53','E80','E90','E81','E132','E157','E148',
-                                  'E137','E125','E149','E116'])
-evoked_go = epochs_go['go/nogo'].average()
-evoked_go.plot(titles='Go/nogo task evoked response', spatial_colors=True, gfp=True)
-evoked_go.plot_topomap(title='Go/nogo task evoked response power map over time')
+mne.combine_evoked([left, -right], weights='equal').plot_joint(times=np.arange(-0.02,0.075,0.015))
 
